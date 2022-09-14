e97dc74782354eef0fb9d84a57b19c44e3421e2a
ebuyproject <projectebuy@gmail.com>
2022-09-14 22:43:31 +0530

    Add By Bhumi

---
+++ java.java
@@ -0,0 +1,10 @@
+import java.util.*;  
+class java {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // System.in is a standard input stream
+        System.out.print("Enter Name: ");
+        String str = sc.nextLine();
+        System.out.print("Enter Branch: ");
+        String str1 = sc.nextLine();
+    }
+}
